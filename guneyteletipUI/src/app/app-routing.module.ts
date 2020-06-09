196bd239ffc0d617c8693ef8ab98e31d99240b6e
mmagaoglu <mhmmdmrt@hotmail.com>
2020-06-09 13:12:50 +0300

    Temel sayfalar eklendi (login, dashboard, sidelayout(header, sidemenu, footer))

--- guneyteletipUI/src/app/app-routing.module.ts
+++ guneyteletipUI/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SidelayoutComponent } from './Shared/Layouts/sidelayout/sidelayout.component';
+import { DashboardComponent } from './Features/Private/Dashboard/Pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SidelayoutComponent,
+    children: [
+      { path: '', component: DashboardComponent, pathMatch: 'full' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- guneyteletipUI/src/app/app.module.ts
+++ guneyteletipUI/src/app/app.module.ts
@@ -3,15 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidelayoutComponent } from './Shared/Layouts/sidelayout/sidelayout.component';
+import { HeaderComponent } from './Shared/Layouts/sidelayout/header/header.component';
+import { SidemenuComponent } from './Shared/Layouts/sidelayout/sidemenu/sidemenu.component';
+import { FooterComponent } from './Shared/Layouts/sidelayout/footer/footer.component';
+import { LoginComponent } from './Features/Public/Login/Pages/login/login.component';
+import { DashboardComponent } from './Features/Private/Dashboard/Pages/dashboard/dashboard.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidelayoutComponent,
+    HeaderComponent,
+    SidemenuComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
   providers: [],
   bootstrap: [AppComponent]
 })
