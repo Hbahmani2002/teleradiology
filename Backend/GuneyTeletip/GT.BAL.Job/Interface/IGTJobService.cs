c50be0a41e66a6d32e62e5056a7442d7745066a0
cagdas <cagdas@CAGDASAKDENIZ>
2020-06-17 10:26:45 +0300

    jobmanager1

---
+++ Backend/GuneyTeletip/GT.BAL.Job/Interface/IGTJobService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GT.BAL.Job.Interface
+{
+    interface IGTJobDataService
+    {
+        IEnumerable<IJobParamterModel> GetInfinityJobParamterList(long lastId);
+    }
+
+    public interface IJobParamterModel
+    {
+        int Interval { get; set; }
+        string name { get; set; }
+        long tenat_fk { get; set; }
+     
+
+
+
+
+    }
+}
