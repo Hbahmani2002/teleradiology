bbf1d2b36eaa8a68e78421dcfc6b323652d5b98c
ProtekWS <ProtekWS@DESKTOP-K3F0U21>
2020-06-18 15:44:37 +0300

    Teletip Kos commandLine Service implamentation

---
+++ Backend/GuneyTeletip/KOS.TeletipKos/Model/MakeKosParameter.cs
@@ -0,0 +1,38 @@
+﻿namespace GT.TeletipKos
+{
+    public class MakeKosParameter
+    {
+        public MakeKosParameter(string title, string institutionName, string institutionSKRS, string institutionFirmaKodu, string locationUid, string tempDirectoryPath, string dcmDirectoryPath, string accessionNumber, string patientSex, string patientId, string inputStudyDirectoryPath, string outputKosFilePath)
+        {
+            Title = title;
+            InstitutionName = institutionName;
+            InstitutionSKRS = institutionSKRS;
+            InstitutionFirmaKodu = institutionFirmaKodu;
+            LocationUid = locationUid;
+            TempDirectoryPath = tempDirectoryPath;
+            DcmDirectoryPath = dcmDirectoryPath;
+            AccessionNumber = accessionNumber;
+            PatientSex = patientSex;
+            PatientId = patientId;
+            InputStudyDirectoryPath = inputStudyDirectoryPath;
+            OutputKosFilePath = outputKosFilePath;
+        }
+
+        public string Title { get; }
+        public string InstitutionName { get; }
+        public string InstitutionSKRS { get; }
+        public string InstitutionFirmaKodu { get; }
+        public string LocationUid { get; }
+        public string TempDirectoryPath { get; }
+        public string DcmDirectoryPath { get; }
+        public string AccessionNumber { get; }
+        public string PatientSex { get; }
+        public string PatientId { get; }
+        public string InputStudyDirectoryPath { get; }
+        public string OutputKosFilePath { get; }
+
+       
+    }
+
+
+}
---
+++ Backend/GuneyTeletip/KOS.TeletipKos/Model/SendKosParameter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GT.TeletipKos.Model
+{
+    public class SendKosParameter
+    {
+        public SendKosParameter(string patientId, string kosFilePath, string serviceUrl)
+        {
+            PatientId = patientId;
+            KosFilePath = kosFilePath;
+            ServiceUrl = serviceUrl;
+        }
+
+        public string PatientId { get; }
+        //RepositoryPath
+        public string KosFilePath { get; }
+        public string ServiceUrl { get; }
+
+
+    }
+}
