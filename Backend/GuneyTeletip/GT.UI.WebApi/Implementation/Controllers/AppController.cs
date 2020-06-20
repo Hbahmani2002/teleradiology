86f0d8de4b007d8ddaed182c2eb6f16bbc9b4a57
ProtekWS <ProtekWS@DESKTOP-K3F0U21>
2020-06-20 09:14:22 +0300

    UI Webapi Null value json model binding test added

--- Backend/GuneyTeletip/GT.UI.WebApi/Implementation/Controllers/AppController.cs
+++ Backend/GuneyTeletip/GT.UI.WebApi/Implementation/Controllers/AppController.cs
@@ -46,7 +46,7 @@ namespace GT.UI.WebApi.Controllers
         }
 
         [Route("/App/Settings")]
-        public ServiceResult<object> Test()
+        public ServiceResult<object> Settings()
         {
             var basePath = Environment.CurrentDirectory;
             var kosPath = Path.GetFullPath("../teletip_kos/", basePath);
@@ -59,7 +59,8 @@ namespace GT.UI.WebApi.Controllers
             ProcessResult res = null;
             try
             {
-                res = ProcessUtil.Start("java", processParameter, WorkingDirectory);
+                //res = ProcessUtil.Start("java", processParameter, WorkingDirectory);
+                res = ProcessUtil.Start("java", processParameter);
             }
             catch (Exception exp)
             {
@@ -73,24 +74,43 @@ namespace GT.UI.WebApi.Controllers
                 WorkingDirectory = WorkingDirectory,
                 Result = res,
                 Ex = ex,
-            }); 
+            });
 
         }
         public string GetInfo()
         {
 
-            var MakeKosJarName = "MakeKOS_v21.jar";
-            var output = $"/dicom/kos/test_{Guid.NewGuid().ToString().Replace("-", "")}.dcm";
-            var input = "/dicom/study/YKCK01/20200519/E0071610";
-            var makeKosAppLocation = "/app/teletip_kos/MakeKOS_v21.jar";
-            var tempTLocation = "/dicom/temp_kos";
-            var dcmLocation = "/app/teletip_kos/dcm4che-5.22.2/bin";
-            var res = $@"{makeKosAppLocation} --title DCM-113030 --location-uid  1.3.6.1.4.1.21367.2017.10.26.111 --temp-tlocation {tempTLocation} --dcm-dcmlocation {dcmLocation} -o {output} {input}";
+            var makeKosJarName = "MakeKOS_v21";
+            var makeKosAppLocation = $"/gt/app/teletip_kos/{makeKosJarName}.jar";
+
+            var output = $"/gt/dicom/kos/test_{Guid.NewGuid().ToString().Replace("-", "")}.dcm";
+            var input = "/gt/dicom/study/YKCK01/20200519/E0071610";
+
+            var tempTLocation = "/gt/dicom/temp_kos";
+            var dcmLocation = "/gt/app/teletip_kos/dcm4che-5.22.2/bin";
+            var res = $@" --title DCM-113030 --location-uid  1.3.6.1.4.1.21367.2017.10.26.111 --temp-tlocation {tempTLocation} --dcm-dcmlocation {dcmLocation} -o {output} {input}";
             //java -jar /app/dicom/MakeKOS_v21.jar --title DCM-113030 --location-uid  1.3.6.1.4.1.21367.2017.10.26.111 --temp-tlocation /app/dicom/temp --dcm-dcmlocation /app/dicom/dcm4che-5.22.2/bin -o /app/dicom/output/test.dcm /app/dicom/dicom-input-files
-            var processStr = string.Format("-jar {1} {0}", res, MakeKosJarName);
+            var processStr = $"-jar {makeKosAppLocation} {res}";
             return processStr;
 
         }
 
+        public class ModelItem
+        {
+            public long? ID { get; set; }
+            public string Name { get; set; }
+            //public DateTime? Date { get; set; }
+            public bool HasValue { get; set; }
+
+        }
+
+        [HttpPost]
+        [Route("/App/ModelTest")]
+        public ServiceResult<ModelItem> ModelTest(ModelItem item)
+        {
+
+            return HttpMessageService.Ok(item);
+
+        }
     }
 }
