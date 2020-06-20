831064f67545edb8c0bfbe7c81f9901af893ab55
ProtekWS <ProtekWS@DESKTOP-K3F0U21>
2020-06-20 17:06:44 +0300

    background service implementation

---
+++ Backend/GuneyTeletip/KOS.TeletipKos/ThreadManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace KOS.TeletipKos
+{
+    public static class ThreadManager
+    {
+        static ThreadManager()
+        {
+            //ThreadPool.SetMaxThreads(10,100)
+        }
+        public static long PendingWorkItemCount => ThreadPool.PendingWorkItemCount;
+
+        public static Tuple<int, int> MaxThreads
+        {
+            get
+            {
+                ThreadPool.GetMaxThreads(out int a1, out int a2);
+                var t = new Tuple<int, int>(a1, a2);
+                return t;
+            }
+        }
+        public static Tuple<int, int> AvailableThreads
+        {
+            get
+            {
+
+                ThreadPool.GetAvailableThreads(out int a1, out int a2);
+                var t = new Tuple<int, int>(a1, a2);
+                return t;
+            }
+        }
+
+        public static bool QueueUserWorkItem(WaitCallback item)
+        {
+            return ThreadPool.QueueUserWorkItem(item);
+        }
+
+    }
+}
