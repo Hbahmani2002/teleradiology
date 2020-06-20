f605010a009f1f429bbfee72a698b7a5d2c81ddf
ProtekWS <ProtekWS@DESKTOP-K3F0U21>
2020-06-20 12:11:59 +0300

    KosService makekos test added

--- Backend/GuneyTeletip/GT.UI.WebApi/Implementation/Controllers/AppController.cs
+++ Backend/GuneyTeletip/GT.UI.WebApi/Implementation/Controllers/AppController.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using System.Reflection.Emit;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using GT.Core.Settings;
 using GT.DataService.Implementation;
 using GT.DataService.Model;
 using GT.Repository.Models.Filter;
@@ -37,63 +38,17 @@ namespace GT.UI.WebApi.Controllers
             return HttpMessageService.Ok((object)new
             {
                 Version = ass.Version.ToString(),
-                CurrentDirectory = Environment.CurrentDirectory,
-                MachineName = Environment.MachineName,
-                WorkingSet = Environment.WorkingSet,
-                MakeKosPath = Path.Combine(kosPath, "MakeKOS_v21.jar"),
-                SendKosPath = Path.Combine(kosPath, "SendKOS_v15.jar"),
+                Environment = new
+                {
+                    CurrentDirectory = Environment.CurrentDirectory,
+                    MachineName = Environment.MachineName,
+                    WorkingSet = Environment.WorkingSet,
+                },
+                Settings=AppSettings.GetCurrent()
             });
         }
 
-        [Route("/App/Settings")]
-        public ServiceResult<object> Settings()
-        {
-            var basePath = Environment.CurrentDirectory;
-            var kosPath = Path.GetFullPath("../teletip_kos/", basePath);
-            var makeKosPath = Path.Combine(kosPath, "MakeKOS_v21.jar");
-
-            var WorkingDirectory = "/dicom";
-            var processParameter = GetInfo();
-
-            Exception ex = null;
-            ProcessResult res = null;
-            try
-            {
-                //res = ProcessUtil.Start("java", processParameter, WorkingDirectory);
-                res = ProcessUtil.Start("java", processParameter);
-            }
-            catch (Exception exp)
-            {
-                ex = exp;
-            }
-
-
-            return HttpMessageService.Ok((object)new
-            {
-                ProcessParameter = processParameter,
-                WorkingDirectory = WorkingDirectory,
-                Result = res,
-                Ex = ex,
-            });
-
-        }
-        public string GetInfo()
-        {
-
-            var makeKosJarName = "MakeKOS_v21";
-            var makeKosAppLocation = $"/gt/app/teletip_kos/{makeKosJarName}.jar";
-
-            var output = $"/gt/dicom/kos/test_{Guid.NewGuid().ToString().Replace("-", "")}.dcm";
-            var input = "/gt/dicom/study/YKCK01/20200519/E0071610";
-
-            var tempTLocation = "/gt/dicom/temp_kos";
-            var dcmLocation = "/gt/app/teletip_kos/dcm4che-5.22.2/bin";
-            var res = $@" --title DCM-113030 --location-uid  1.3.6.1.4.1.21367.2017.10.26.111 --temp-tlocation {tempTLocation} --dcm-dcmlocation {dcmLocation} -o {output} {input}";
-            //java -jar /app/dicom/MakeKOS_v21.jar --title DCM-113030 --location-uid  1.3.6.1.4.1.21367.2017.10.26.111 --temp-tlocation /app/dicom/temp --dcm-dcmlocation /app/dicom/dcm4che-5.22.2/bin -o /app/dicom/output/test.dcm /app/dicom/dicom-input-files
-            var processStr = $"-jar {makeKosAppLocation} {res}";
-            return processStr;
-
-        }
+       
 
         public class ModelItem
         {
@@ -108,9 +63,7 @@ namespace GT.UI.WebApi.Controllers
         [Route("/App/ModelTest")]
         public ServiceResult<ModelItem> ModelTest(ModelItem item)
         {
-
             return HttpMessageService.Ok(item);
-
         }
     }
 }
