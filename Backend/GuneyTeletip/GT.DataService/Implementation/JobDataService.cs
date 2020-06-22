c676402bfa93c522d4390498ad0bcf8294b197b4
zehra kovar <zehra_282@hotmail.com>
2020-06-22 15:53:30 +0300

    jobController eklendi

---
+++ Backend/GuneyTeletip/GT.DataService/Implementation/JobDataService.cs
@@ -0,0 +1,30 @@
+﻿using Gt.PERSISTANCE;
+using GT.DataService.Model;
+using GT.Repository.Models.View;
+using GT.SERVICE;
+using GT.UTILS.GRID;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GT.DataService.Implementation
+{
+    public class JobDataService : BaseService
+    {
+        AbstractWorkspace _Workspace;
+        public JobDataService(IBussinessContext context) : base(context)
+        {
+            _Workspace = GTWorkspaceFactory.Create(true);
+        }
+
+        public PagingResult<JobViewmodel> GetJobList(Gridable<JobViewFilter> parms)
+        {
+            return null;
+        }
+
+        public int Save(DateTime basTar, DateTime bitTar, string tip, long basariliSayisi, long basarisizSayisi)
+        {
+            return 1;
+        }
+    }
+}
---
+++ Backend/GuneyTeletip/GT.UI.WebApi/Models/Data/Job/SaveJobModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GT.UI.WebApi.Models.Data.Job
+{
+    public class SaveJobModel
+    {
+        public DateTime basTar { get; set; }
+        public DateTime bitTar { get; set; }
+        public string tip { get; set; } 
+        public long basariliSayisi { get; set; } 
+        public long basarisizSayisi { get; set; }
+    }
+}
