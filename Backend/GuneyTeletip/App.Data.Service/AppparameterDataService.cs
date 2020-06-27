3ac032b84300f303306da28caf2fb43ce85fdc50
zehra kovar <zehra_282@hotmail.com>
2020-06-27 15:31:23 +0300

    AppparameterDataService olusturuldu

---
+++ Backend/GuneyTeletip/App.Data.Service/AppparameterDataService.cs
@@ -0,0 +1,22 @@
+﻿using Gt.PERSISTANCE;
+using GT.Repository.Implementation;
+using GT.SERVICE;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Data.Service
+{
+    public class AppparameterDataService : BaseService
+    {
+        AppParameterRepository appParameterRepository;
+        public AppparameterDataService() : this(null)
+        {
+        }
+        public AppparameterDataService(IBussinessContext context) : base(context)
+        {
+            _Workspace = GTWorkspaceFactory.Create(true);
+            appParameterRepository = new AppParameterRepository(_Workspace);
+        }
+    }
+}
---
+++ Backend/GuneyTeletip/GT.Repository/Implementation/AppParametterRepository.cs
@@ -0,0 +1,22 @@
+﻿using Gt.PERSISTANCE;
+using GT.Persistance.Domain.Models;
+using GT.REPOSITORY;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GT.Repository.Implementation
+{
+    public class AppParameterRepository : AbstractTableRepository<AppParameter>
+    {
+        public AppParameterRepository(IAbstractWorkspace workspace) : base(workspace)
+        {
+
+        }
+
+        public override AppParameter GetByID(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
