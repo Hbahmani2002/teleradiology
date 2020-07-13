f502438a4b5d03a20d40afbdb2d379dc529321e7
zehra kovar <zehra_282@hotmail.com>
2020-07-13 21:14:33 +0300

    Dcm4chee için persistance, repository ve dataservice oluşturuldu

---
+++ Backend/GuneyTeletip/Dcm4chee.Persistance.Domain/Models/RelUpsPerfCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dcm4chee.Persistance.Domain.Models
+{
+    public partial class RelUpsPerfCode
+    {
+        public long UpsFk { get; set; }
+        public long PerfCodeFk { get; set; }
+
+        public virtual Code PerfCodeFkNavigation { get; set; }
+        public virtual Ups UpsFkNavigation { get; set; }
+    }
+}
