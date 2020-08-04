40f0a7cd01acd56899309c8a014b4ce2e34ee136
zehra <USER@DESKTOP-S76CKLA>
2020-08-04 16:52:37 +0300

    CreateKos düzeltme

--- Backend/GuneyTeletip/GT.UI.WebApi/Implementation/Controllers/KosStudyController.cs
+++ Backend/GuneyTeletip/GT.UI.WebApi/Implementation/Controllers/KosStudyController.cs
@@ -39,7 +39,7 @@ namespace GT.UI.WebApi.Controllers
             if (parms.Filter.BitTarih.HasValue)
                 parms.Filter.BitTarih = parms.Filter.BitTarih.Value.AddDays(1).AddHours(2).AddMinutes(59).AddSeconds(59);
             var sd = new StudyKosService(GetBussinesContext());
-            var job = sd.CreateKos(parms);
+            var job = sd.CreateKosJson(parms);
             return HttpMessageService.Ok(job);
         }
 
