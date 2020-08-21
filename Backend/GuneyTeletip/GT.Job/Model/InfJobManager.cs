6fa3a4514ba26927d0083df625514a17474c56ce
cagdas <cagdas@CAGDASAKDENIZ>
2020-08-21 16:05:12 +0300

    guney.

--- Backend/GuneyTeletip/GT.Job/Model/InfJobManager.cs
+++ Backend/GuneyTeletip/GT.Job/Model/InfJobManager.cs
@@ -112,9 +112,9 @@ namespace GT.Job.Implementation
 
 
 
-            //dt.SyncronizeInfinityStudyList(item.FkTenant.Value, item.OracleStudyKeyLast.Value, item.TimeStart, item.TimeStop);
+            dt.SyncronizeInfinityStudyList(item.FkTenant.Value, item.OracleStudyKeyLast.Value, item.TimeStart, item.TimeStop);
 
-            dt.SyncronizeProtekStudyList(item.FkTenant.Value, item.OracleStudyKeyLast.Value, item.TimeStart, item.TimeStop);
+            //dt.SyncronizeProtekStudyList(item.FkTenant.Value, item.OracleStudyKeyLast.Value, item.TimeStart, item.TimeStop);
 
             
 
